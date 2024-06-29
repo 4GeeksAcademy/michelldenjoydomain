6b6c0ef775a26307f513b102d213ce6571652e74
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-06-29 17:51:07 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  function generadordedominio() {
+    let pronombres = ["the", "this", "that"];
+    let adjetivos = ["free", "amazing", "efficiente"];
+    let nombres = ["webside", "course", "academy"];
+    let dominios = [];
+
+    pronombres.forEach(pronombre => {
+      adjetivos.forEach(adjetivo => {
+        nombres.forEach(nombre => {
+          dominios.push(pronombre + adjetivo + nombre + ".com");
+        });
+      });
+    });
+    return dominios.join("<br>");
+  }
+
+  document.getElementById(
+    "generadordedominio"
+  ).innerHTML = generadordedominio();
 };
